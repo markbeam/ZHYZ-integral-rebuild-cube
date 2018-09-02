99104dbdd75de85c5217a0a091368d722e039c46
mark <20787403+markbeam@users.noreply.github.com>
2018-09-02 20:56:03 +0800

    change api root address

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,6 +4,6 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_ROOT: '"//10.21.168.65:9054/jifen2/api.php?s="'
-  // API_ROOT: '"//139.199.1.240/jifen2/api.php?s="'
+  // API_ROOT: '"//10.21.168.65:9054/jifen2/api.php?s="'
+  API_ROOT: '"//139.199.1.240/jifen2/api.php?s="'
 })
